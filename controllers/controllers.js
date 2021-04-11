904c1ca93e6b371c6e60d8d2d5421b33835436db
Corvus-AI <180010031@iitdh.ac.in>
2021-04-11 23:00:22 +0530

    corrected database , working now

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,6 +1,7 @@
 
 const getBalance = (req, res, next) => {
 var mysql = require('mysql');
+var bal ;
 
 var con = mysql.createConnection({
   host: "remotemysql.com",
@@ -13,35 +14,34 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+  con.query("SELECT Balance FROM Details where Username=\"test\" ", function (err, result, fields) {
+  if (err) throw err;
+  bal = result;
+  console.log(result);
+});
 });
 
-/* con.connect(function(err) {
-    if (err) throw err;
-    con.query("SELECT balance FROM bank where user= ", function (err, result, fields) {
-      if (err) throw err;
-      console.log(result);
-    });
-  }); */
-
     res.status(200).json({
-        balance: "Balance from Cloudant will be returned here"
+        balance: bal
     });
 };
 
 const addFunds = (req, res, next) => {
     /* TODO updating bank balance in Database */ 
-    var mysql = require('mysql');
+var mysql = require('mysql');
+add = 100 ;
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "yourusername",
-  password: "yourpassword",
-  database: "mydb"
+  host: "remotemysql.com",
+  user: "nGgOkH1PqW",
+  password: "CQBZveV1zb",
+  port: "3306" ,
+  database: 'nGgOkH1PqW'
 });
 
 con.connect(function(err) {
   if (err) throw err;
-  var sql = "UPDATE bank SET balance += add WHERE user = 'Valley 345'";
+  var sql = "UPDATE Details SET Balance += 100 WHERE Username=\"test\" ";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log(result.affectedRows + " record(s) updated");
